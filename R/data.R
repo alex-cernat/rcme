63efed0ee3457c8253fad8d7930e0b62aad61419
alex-cernat <alexandru.cernat@manchester.ac.uk>
2022-06-21 09:56:19 +0100

    beta data documentation

---
+++ R/data.R
@@ -0,0 +1,34 @@
+#' A synthetic data of crime damage.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 250 rows and 6 variables:
+#' \describe{
+#'   \item{collective_efficacy}{}
+#'   \item{unemployment}{}
+#'   \item{median_age}{}
+#'   \item{white_british}{}
+#'   \item{damage_crime}{}
+#'   \item{log_damage_crime}{}
+#' }
+#' @source \url{http://www.diamondse.info/}
+"crime_damage"
+
+
+#' A synthetic data of crime disorder.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{violent_crime}{}
+#'   \item{white_british}{}
+#'   \item{unemployment}{}
+#'   \item{median_age}{}
+#'   \item{disorder}{}
+#'   \item{log_violent_crime}{}
+#' }
+#' @source \url{http://www.diamondse.info/}
+"crime_disorder"
